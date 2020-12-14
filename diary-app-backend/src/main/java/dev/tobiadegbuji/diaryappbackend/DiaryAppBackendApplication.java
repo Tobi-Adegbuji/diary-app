b0cca03732c03014457520a0651b491f902547c9
Tobi <adegbuji_tobi@yahoo.com>
2020-12-14 10:45:20 -0500

    init

---
+++ diary-app-backend/src/main/java/dev/tobiadegbuji/diaryappbackend/DiaryAppBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.tobiadegbuji.diaryappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryAppBackendApplication.class, args);
+    }
+
+}
---
+++ diary-app-backend/src/test/java/dev/tobiadegbuji/diaryappbackend/DiaryAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tobiadegbuji.diaryappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
