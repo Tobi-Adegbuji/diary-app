1d9a44018fb38115222b19870d7c85dfdf043a45
Tobi <adegbuji_tobi@yahoo.com>
2020-12-14 16:16:04 -0500

    NavBar Component Created

--- diary-app-frontend/diary-app-frontend/src/App.js
+++ diary-app-frontend/diary-app-frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/login/Login'
+import NavBar from './components/navigation/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <NavBar className="bottom"/>
     </div>
   );
 }
