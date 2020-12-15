36d11266fbca3aa62d43ce83e9699017f8d665dc
Tobi <adegbuji_tobi@yahoo.com>
2020-12-14 19:22:24 -0500

    Added Diary Component

--- diary-app-frontend/diary-app-frontend/src/components/AppContainer/SideNav/SideNav.jsx
+++ diary-app-frontend/diary-app-frontend/src/components/AppContainer/SideNav/SideNav.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
-import './SideNav.css'
+import React from "react";
+import Diary from "./diary/Diary";
+import "./SideNav.css";
 
 function SideNav() {
-    return (
-        
-            <nav id="mySideNav" className="sideNav">
-            <img className="profile_img" src="https://www.pngkey.com/png/full/73-730477_first-name-profile-image-placeholder-png.png"/>
-            <p>John Doe</p>
-            <div className="nav_divider_top"></div>
-            <a href="#">Home</a>
-            <a href="#">Explore</a>
-            <a href="#">My Collection</a>
-            <a href="#">Profile</a>
-            <div className="nav_divider_bottom"></div>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-            <a href="#">Log out</a>
-
-
-        </nav>
-        
-    )
+  return (
+    <nav id="mySideNav" className="sideNav">
+      <img
+        className="profile_img"
+        src="https://www.pngkey.com/png/full/73-730477_first-name-profile-image-placeholder-png.png"
+      />
+      <p>John Doe</p>
+      <div className="nav_divider_top"></div>
+      <a href="#">Add Diary</a>
+      <Diary />
+    </nav>
+  );
 }
 
-export default SideNav
+export default SideNav;
--- diary-app-frontend/diary-app-frontend/src/components/navigation/NavBar.jsx
+++ diary-app-frontend/diary-app-frontend/src/components/navigation/NavBar.jsx
@@ -1,14 +1,12 @@
-
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FolderIcon from '@material-ui/icons/Folder';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import Box from '@material-ui/core/Box';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FolderIcon from "@material-ui/icons/Folder";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles({
   root: {
@@ -18,20 +16,48 @@ const useStyles = makeStyles({
 
 export default function LabelBottomNavigation() {
   const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState("recents");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box boxShadow={4} justifyContent="center" alignItems="center" display="flex" className="bottom">
-    <BottomNavigation value={value} onChange={handleChange} className={classes.root} >
-      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
+    <Box
+      boxShadow={4}
+      justifyContent="center"
+      alignItems="center"
+      display="flex"
+      className="bottom"
+    >
+      <BottomNavigation
+        value={value}
+        onChange={handleChange}
+        className={classes.root}
+      >
+        <BottomNavigationAction
+          label="Diaries"
+          value="recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOnIcon />}
+        />
+        <BottomNavigationAction
+          label="Folder"
+          value="folder"
+          icon={<FolderIcon />}
+        />
+
+        <a href="#">Sign Out</a>
+      </BottomNavigation>
     </Box>
   );
 }
